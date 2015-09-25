7096604356e2c09d7a7656cba313e9dd1e90c100
Scott Harsla <harsla@gmail.com>
2015-09-25 13:54:15 -0700

    controllers

--- app/app.js
+++ app/app.js
@@ -6,15 +6,17 @@ angular.module('myApp', [
 ])
 .config(function($stateProvider, $urlRouterProvider) {
   //For any unmatched url, redirect to /state1
-  $urlRouterProvider.otherwise("/");
+  $urlRouterProvider.otherwise('/');
   
   $stateProvider
   .state('view1', {
-    url: "/view1",
-    templateUrl: "view1/view1.html"
+    url: '/view1',
+    templateUrl: 'view1/view1.html',
+    controller: 'view1/view1.js'
   })
   .state('view2', {
-    url: "/view2",
-    templateUrl: "view2/view2.html"
+    url: '/view2',
+    templateUrl: 'view2/view2.html',
+    controller: 'view2/view2.js'
   });
 });
